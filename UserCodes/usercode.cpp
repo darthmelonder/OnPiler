5c98a21575c2ce24d849706d066941f9a95a6e26
Jatin <darthmelonder@gmail.com>
2018-10-14 23:28:56 +0530

    CSS Styling

--- UserCodes/usercode.cpp
+++ UserCodes/usercode.cpp
@@ -1,13 +1,1 @@
- #include <iostream>
-#include <vector>
-using namespace std;
-vector <int> vec;
-int main()
-{
-    //while (1){}
-  int j;
-  cin>>j;
-  vec.push_back(j);
- cout<<vec[0];
-    return 0;
-}
+ 
